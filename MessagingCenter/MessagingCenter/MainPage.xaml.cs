b5c3dfc43b0f4bb5a6edae4411da808549bcc94b
RonyZe <zrony58@gmail.com>
2019-01-23 20:06:12 +0500

    Add project files.

---
+++ MessagingCenter/MessagingCenter/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessagingCenter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnClick(object sender, System.EventArgs e)
+        {
+            var page = new TargetPage();
+            page.SliderValueChanged += OnSliderValueChanged;
+            Navigation.PushAsync(page);
+        }
+
+        private void OnSliderValueChanged(object source, double newValue)
+        {
+            label.Text = newValue.ToString();
+        }
+    }
+}
---
+++ MessagingCenter/MessagingCenter/TargetPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessagingCenter
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TargetPage : ContentPage
+    {
+        public event EventHandler<double> SliderValueChanged;
+		public TargetPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValueChanged?.Invoke(this, e.NewValue);           
+        }
+    }
+}
